af787cf500d05e4b9efb9c8c7f6e51ada836092a
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2014-09-27 15:17:07 -0700

    Install a lot of packages

--- install.sh
+++ install.sh
@@ -25,6 +25,8 @@ esac
 
 ensure_brew
 
+ensure_package macvim -- --override-system-vim
+ensure_package screen
 ensure_package git
 ensure_package hub
 ensure_package ack
@@ -37,9 +39,19 @@ ensure_package tree
 ensure_package unrar
 ensure_package ctags
 ensure_package wget -- --default-names
-ensure_package grep -- --default-names
 ensure_package imagemagick
 ensure_package arp-scan
+ensure_package graphviz
+ensure_package phantomjs
+ensure_package ansible
+ensure_package go
+ensure_package htop
+
+ensure_tap educabilia/tooling
+ensure_package bs
+
+ensure_tap homebrew/dupes
+ensure_package grep -- --default-names
 
 ensure_package libxslt --link
 ensure_package libxml2 --link
@@ -47,12 +59,16 @@ ensure_package libiconv --link
 ensure_package readline --link
 ensure_package gnu-getopt --link
 ensure_package openssl --link
+ensure_package icu4c --link
 
 ensure_package postgresql --service
 ensure_package memcached --service
 ensure_package redis --service
+ensure_package dnsmasq --service
+cp /usr/local/opt/dnsmasq/dnsmasq.conf.example /usr/local/etc/dnsmasq.conf
 
-ensure_ruby "2.1.0"
+ensure_ruby "2.1.2"
 
 ensure_package node
 ensure_npm bower
+ensure_npm localtunnel
